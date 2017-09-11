734ba15609b2bbc20c37f618ab0ebff610e6701e
user <tya.hsarme@gmail.com>
2017-09-11 08:32:17 +0300

    ;kkl;k;

--- app/src/main/java/Teya.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by user on 9/11/2017.
- */
-public class Teya {
-}
--- app/src/main/java/com/hsarme/teya/teya121taskmnqr2017/Teya.java
+++ app/src/main/java/com/hsarme/teya/teya121taskmnqr2017/Teya.java
@@ -3,5 +3,5 @@ package com.hsarme.teya.teya121taskmnqr2017;
 /**
  * Created by user on 9/11/2017.
  */
-public class Tete {
+public class Teya {
 }
